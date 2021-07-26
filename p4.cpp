b803d8f2cdac97b94341765944a8534f6328140d
Kajal <kajal832jha@gmail.com>
2021-07-26 16:40:07 +0530

    initial commit

---
+++ p4.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+    int a=10;
+    int *p;
+   
+    p=(int *)malloc(5*(sizeof(int))); //in C
+    
+    p[0]=8;p[1]=3;p[2]=8;p[3]=3;p[4]=8;
+
+    for(int i=0;i<5;i++){
+        cout<<p[i]<<endl;
+
+    }
+    int *d;
+    d= new int[5]; //memory allocated in heap area ....This way is used in C++ programs
+    cout<<d[4];  //this will give us a garbage value ....
+
+    
+}
