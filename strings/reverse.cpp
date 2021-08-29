c1d68f23c9206a669e0f2cff8aa4f8be2806a1e9
Kajal <kajal832jha@gmail.com>
2021-08-29 20:15:43 +0530

    initial commit

---
+++ strings/reverse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int reverse(string str ){
+    int i,j;
+    char t;
+    for(i=0;str[i]!='\0';i++){}
+    i=i-1;
+    for(j=0;i>j;i--,j++){
+        t=str[j];
+        str[j]=str[i];
+        str[i]=t;
+    }
+    cout<<str;
+}
+int main(){
+    string str="kajal";
+    reverse(str);
+
+}
