48d8010e99d50b99931e2c4e0a158853fc9f05f5
Kajal <kajal832jha@gmail.com>
2021-09-06 13:42:58 +0530

    insertion in sorted LL

--- LinkedList.cpp/inserting.cpp
+++ LinkedList.cpp/inserting.cpp
@@ -36,11 +36,43 @@ int Insert(struct Node *p,int val){
 
 
 }
+//insertion in sorted linked list
+int sortedInsertion(struct Node *p,int val){
+    struct Node *q=NULL;
+    struct Node *t;
+    t=(struct Node *)malloc(sizeof(struct Node *));
+    t->data=val;
+    t->next=NULL;
+    
+    if(first==NULL){        //if the linked list is totally empty
+        first=t;
+
+    }
+    else{while(p!=NULL && p->data<val){
+        q=p;
+        p=p->next;
+    }
+    if(p==first){         //if there is only one node in a linked list
+        t->next=first;
+        first=t;
+
+    }
+      else{                  //for inserting in between or at the end of the linked list
+          t->next=q->next;
+           q->next=t;
+   }
+    }
+}
 int main(){
-    Insert(first,7);
+    Insert(first,5);
     Insert(first,8);
     Insert(first,10);
-    Insert(first,6);
+    Insert(first,16);
+    sortedInsertion(first,11);
+    sortedInsertion(first,12);
+    sortedInsertion(first,6);
+    sortedInsertion(first,4);
+    sortedInsertion(first,18);
     Display(first);
     return 0;
 }
