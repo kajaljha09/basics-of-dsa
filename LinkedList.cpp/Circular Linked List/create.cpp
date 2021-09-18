2c60177b57e6473aa2fc1543c4f004df7021ed6c
Kajal Jha <kajal832jha@gmail.com>
2021-09-18 09:43:02 +0530

    insertion

--- LinkedList.cpp/Circular Linked List/create.cpp
+++ LinkedList.cpp/Circular Linked List/create.cpp
@@ -34,11 +34,58 @@ void Display(struct Node *p){
 
 
 }
+void RDisplay(struct Node *p){
+    static int flag=0;
+    if(p!=head || flag==0){
+        flag=1;
+        cout<<p->data<<" ";
+        Display(p->next);
+
+    }
+    flag=0;
+
+}
+//insertion in circular linked list is of two types :
+//insertion at any position 
+//insertion before head node
+void Insertion(struct Node *p,int pos,int x){
+    struct Node *t;
+    if(pos==0){          //insertion before head
+     t=new struct Node;
+     t->data=x;
+     if(head==NULL){
+         head=t;
+         head->next=head;
+
+     }
+     else{
+         p=head;
+         while(p->next!=head){
+             p=p->next;
+         }
+         p->next=t;
+         t->next=head;
+         head=t;
+     }
+
+    }
+    else{          //insertion at any position 
+       for(int i=0;i<pos-1;i++){
+           p=p->next;
+       }
+       t=new struct Node;
+       t->data=x;
+       t->next=p->next;
+       p->next=t;
+    }
+}
 int main(){
     int A[]={1,2,3,4,5,6,7};
     int n=sizeof(A)/sizeof(A[0]);
     Create(A,n);
     Display(head);
-
-    
+    cout<<"Display the circular linked list using recursion  ";
+    RDisplay(head);
+    Insertion(head,2,8);   //so basically it is going to insert after the position specified
+    Display(head);
 }
